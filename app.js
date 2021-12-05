88ad08a97cec372e3950b757af0e7b6552c23a5f
Austin1a2b <cavalier283@gmail.com>
2021-12-05 20:03:20 +0800

    基本設定完成

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/pratice-todo-list')
+
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log('localhost:3000')
+})
+
+
